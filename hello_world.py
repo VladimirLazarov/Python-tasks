53b8e2531a41fdeca7d265b4db25eeb63195f550
Vladimir Lazarov Tsvetkov <155313542+VladimirLazarov@users.noreply.github.com>
2024-02-21 15:14:38 +0000

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+#Pseudo code:
+# 1. Ask the user to enter their name using input()
+# 2. Ask the uder to enter their age using input()
+# 3. Print out the entered name and age
+# 4. Print the string "Hello World" on a new line
+
+# Get the user's name 
+name = input("Enter your name: ")
+# get the user's age
+age = input("Enter your age: ")
+# Print the user's name and age
+print (name, age)
+# Print "Hello World" on a new line
+print ("Hello Woeld")
+
+
