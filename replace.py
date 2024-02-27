bcc0bb21aa1377f3f7e9c98232c3349060b1ecdf
Vladimir Lazarov Tsvetkov <155313542+VladimirLazarov@users.noreply.github.com>
2024-02-27 22:22:41 +0000

    Replace.py
    The String and Numerical Data Types.

---
+++ replace.py
@@ -0,0 +1,7 @@
+message = "The!quick!fox!jump!over!the!lazy!dog."
+message_replace = message.replace("!", " ")
+print(message_replace)
+uppercase_message = message_replace.upper()
+print(uppercase_message)
+reverse_message = message_replace[::-1]
+print(reverse_message)
