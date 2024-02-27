ce423e84f51b7bffecca987f8695e37aaa8f0363
Vladimir Lazarov Tsvetkov <155313542+VladimirLazarov@users.noreply.github.com>
2024-02-27 22:32:29 +0000

    age-quiz.py
    Control Structures - If, Elife, Else and the Boolean data type.

---
+++ age-quiz.py
@@ -0,0 +1,15 @@
+user_input = input("Enter your age:")
+age = int(user_input)
+
+if age >= 40:
+    print("You're over the hill")
+elif age > 100:
+    print("Sorry you are dead")
+elif age >= 65:
+    print("Enjoy your retirement!")
+elif age < 13:
+    print("You qualify for the kiddie discount.") 
+elif age == 21:
+    print("Congrats on your 21st!")
+else:
+    print("Age is but a number.")
